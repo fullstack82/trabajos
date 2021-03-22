bd6efb1800c1bea39bf2718bea9b6da23d22d205
fullstack82 <fullstack8210@gmail.com>
2021-03-22 18:17:20 +0100

    empezamos con la variables globales o locales

---
+++ master-fullstack/javascript/letVar.js
@@ -0,0 +1,12 @@
+'use strict';
+// Uso de Var y Let;
+var numero = 40;
+console.log(numero); // valor 40
+
+if (true) {
+    var numero = 30;
+    console.log(numero); // Valor 30
+}
+
+
+console.log(numero); // Valor 30
---
+++ master-fullstack/javascript/variables.js
@@ -0,0 +1,21 @@
+'use strict';
+// Variables = una variable es un contenedor de información 
+
+
+// Diferencias entre let y var
+// Let permite definir variables limitando su alcance al bloque, declaracion o expresion donde se esta utilizando;
+// Var define una variable global o local en una funcion sin importar el ambito del bloque;
+
+
+// Let / Var
+var pais = "españa";
+var continente = "europa";
+var antiguedad = 2019;
+
+console.log(pais, continente, antiguedad);
+
+
+
+let color = "rojo";
+let nombre = "maria";
+
